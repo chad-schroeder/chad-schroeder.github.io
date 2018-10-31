a07151b840cdd9b242c384211d953dff99844e12
Chad Schroeder <gmchad@gmail.com>
2018-10-31 16:18:05 -0700

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+const changePage = event => {
+  if (!event.target.closest('[data-url')) return;
+
+  // get page identity
+  const pageURL = event.target.closest('[data-url]').getAttribute('data-url');
+
+  // hide current page
+  const currentPage = document.querySelector('.page-show');
+  currentPage.classList.remove('page-show');
+
+  // show new page
+  const newPage = document.querySelector('.page-' + pageURL);
+  newPage.classList.add('page-show');
+};
+
+document.addEventListener('click', function(event) {
+  changePage(event);
+});
